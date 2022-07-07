9ed015514564104de1d8d7dd59d8d3ddb5246ea4
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2022-07-07 19:09:35 +0000

    update redeploy script with docker commands

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,11 +3,6 @@
 cd project-team-jungle/
 git fetch && git reset origin/main --hard
 
-source python3-virtualenv/bin/activate
-pip3 install -r requirements.txt	
-
-# restart myportfolio service
-systemctl daemon-reload
-systemctl restart myportfolio
-
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
 
